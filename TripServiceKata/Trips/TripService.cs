6dbea9ab7112c79d98f9429add1bccf2125dc96d
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-02-19 11:19:18 +0200

    Renamed logged in user variable and added some whitespace to improve readability

--- TripServiceKata/Trips/TripService.cs
+++ TripServiceKata/Trips/TripService.cs
@@ -9,22 +9,26 @@ namespace TripServiceKata.Trips
         public List<Trip> GetTripsByUser(User user)
         {
             List<Trip> tripList = new List<Trip>();
-            User loggedUser = UserSession.Instance.GetLoggedUser();
+            User loggedInUser = UserSession.Instance.GetLoggedUser();
+
             bool isFriend = false;
-            if (loggedUser != null)
+
+            if (loggedInUser != null)
             {
                 foreach (User friend in user.Friends)
                 {
-                    if (friend.Equals(loggedUser))
+                    if (friend.Equals(loggedInUser))
                     {
                         isFriend = true;
                         break;
                     }
                 }
+
                 if (isFriend)
                 {
                     tripList = TripDao.FindTripsByUser(user);
                 }
+
                 return tripList;
             }
             else
